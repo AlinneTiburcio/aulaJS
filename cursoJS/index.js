9899857857c7f0926d369ef31b4aa932ded85243
AlinneTiburcio <82852325+AlinneTiburcio@users.noreply.github.com>
2022-10-02 19:14:50 -0300

    classe em JS

---
+++ cursoJS/index.js
@@ -0,0 +1,28 @@
+class Cliente {
+nome;
+cpf;
+agencia;
+saldo;
+rg;
+}
+
+const cliente1 = new Cliente();
+const cliente2 = new Cliente();
+
+cliente1.nome = "Ricardo";
+cliente1.cpf = 1112223309;
+cliente1.agencia = 1001;
+cliente1.saldo = 0;
+cliente1.rg = 4589007;
+
+cliente2.nome = "Alice";
+cliente2.cpf = 8882223309;
+cliente2.agencia = 1001;
+cliente2.saldo = 0;
+
+
+console.log('cliente1');
+
+
+
+
