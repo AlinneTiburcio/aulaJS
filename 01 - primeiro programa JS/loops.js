cb7fa9a6e6f3fbcadfd6a06fa979cfb6d646947d
AlinneTiburcio <82852325+AlinneTiburcio@users.noreply.github.com>
2022-10-18 22:08:17 -0300

    .

--- 01 - primeiro programa JS/loops.js
+++ 01 - primeiro programa JS/loops.js
@@ -5,7 +5,7 @@ const listaDeDestino = new Array(`Salvador`, `São Paulo`, `Rio de Janeiro`)
 const idadeComprador = 22
 const estaAcompanhada = false
 let temPassagemComprada = false
-const destino = 'São Paulo'
+const destino = 'Bor'
 
 console.log('\n Destinos possíveis:')
 console.log(listaDeDestino)
@@ -13,12 +13,22 @@ console.log(listaDeDestino)
 const podeComprar = idadeComprador >= 18 || estaAcompanhada === true
 
 let contador = 0
+let destinoExiste = false;
 
 while (contador < 3) {
+  
   if (listaDeDestino[contador] == destino){
     console.log('Destino existe');
-  } else{
-    console.log('Destino não existe')
-  }
+    destinoExiste = true;
+    break;
+  } 
   contador++
 }
+
+if(podeComprar && destinoExiste){
+  console.log('Boa Viagem!')
+} else {
+  console.log('Não há passagem para esse destino, tente novamente nas opções informado acima!')
+}
+
+
